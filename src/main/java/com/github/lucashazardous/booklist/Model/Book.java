3b0447bf36ef36a9ff616fe0a34bbcc20d5cdf4d
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-02-04 18:41:29 +0100

    book model

---
+++ src/main/java/com/github/lucashazardous/booklist/Model/Book.java
@@ -0,0 +1,42 @@
+package com.github.lucashazardous.booklist.Model;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.Date;
+
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    @MongoId
+    private String id;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private String title;
+    private String author;
+    private int pages;
+    private int currentPage;
+    private boolean finished;
+    private Date addedDate;
+}
