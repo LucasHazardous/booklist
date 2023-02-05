009cfd3bcd44ec63a1ce0680f679c5b55bb490fc
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-02-05 12:59:41 +0100

    improved form for adding with validation

--- src/main/java/com/github/lucashazardous/booklist/Controller/BookController.java
+++ src/main/java/com/github/lucashazardous/booklist/Controller/BookController.java
@@ -34,6 +34,9 @@ public class BookController {
     @PostMapping("/add-book")
     public String addBook(Book book, BindingResult result) {
         if(Objects.equals(book.getTitle(), "")) result.rejectValue("title", "", "Title can't be empty.");
+        if(Objects.equals(book.getAuthor(), "")) result.rejectValue("author", "", "Author can't be empty.");
+        if(book.getPages() <= 0) result.rejectValue("pages", "", "Book must have at least one page.");
+
         if (result.hasErrors()) {
             return "add-book";
         }
