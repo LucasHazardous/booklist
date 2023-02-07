89f6e0065491211c5ee83c6fe68c669b2bf51a7c
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-02-07 13:43:23 +0100

    book creation test

--- src/test/java/com/github/lucashazardous/booklist/BooklistApplicationTests.java
+++ src/test/java/com/github/lucashazardous/booklist/BooklistApplicationTests.java
@@ -1,13 +1,52 @@
 package com.github.lucashazardous.booklist;
 
+import com.github.lucashazardous.booklist.Repository.BookRepository;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BooklistApplicationTests {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-    @Test
-    void contextLoads() {
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class BooklistApplicationTests {
+    private final List<NameValuePair> bookValues;
+
+    {
+        bookValues = new ArrayList<>();
+        bookValues.add(new BasicNameValuePair("id", "test"));
+        bookValues.add(new BasicNameValuePair("title", "Test"));
+        bookValues.add(new BasicNameValuePair("author", "Test"));
+        bookValues.add(new BasicNameValuePair("pages", "100"));
+        bookValues.add(new BasicNameValuePair("currentPage", "1"));
     }
 
+    @Autowired
+    private BookRepository bookRepository;
+
+
+    @Test
+    public void bookCreation() throws IOException, AssertionError {
+        HttpUriRequest request = RequestBuilder.create("POST")
+                .setUri("http://localhost:8080/add-book")
+                .setEntity(new UrlEncodedFormEntity(bookValues))
+                .build();
+
+        long countBefore = bookRepository.count();
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+        long countAfter = bookRepository.count();
+
+        assert httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY;
+        assert countBefore+1 == countAfter;
+    }
 }
