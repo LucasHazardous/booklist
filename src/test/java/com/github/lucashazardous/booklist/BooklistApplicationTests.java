2c79f72d4f669077b84ada7323659554968d2886
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-02-08 17:41:42 +0100

    book deletion test, order tests, id constant

--- src/test/java/com/github/lucashazardous/booklist/BooklistApplicationTests.java
+++ src/test/java/com/github/lucashazardous/booklist/BooklistApplicationTests.java
@@ -9,6 +9,7 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,10 +22,11 @@ import java.util.List;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class BooklistApplicationTests {
     private final List<NameValuePair> bookValues;
+    private final String BOOK_ID = "test";
 
     {
         bookValues = new ArrayList<>();
-        bookValues.add(new BasicNameValuePair("id", "test"));
+        bookValues.add(new BasicNameValuePair("id", BOOK_ID));
         bookValues.add(new BasicNameValuePair("title", "Test"));
         bookValues.add(new BasicNameValuePair("author", "Test"));
         bookValues.add(new BasicNameValuePair("pages", "100"));
@@ -36,6 +38,7 @@ class BooklistApplicationTests {
 
 
     @Test
+    @Order(1)
     public void bookCreation() throws IOException, AssertionError {
         HttpUriRequest request = RequestBuilder.create("POST")
                 .setUri("http://localhost:8080/add-book")
@@ -44,7 +47,20 @@ class BooklistApplicationTests {
 
         HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
 
-        assert bookRepository.findById("test").isPresent();
+        assert bookRepository.findById(BOOK_ID).isPresent();
         assert httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY;
+    }
+
+    @Test
+    @Order(2)
+    public void bookDeletion() throws IOException, AssertionError {
+        HttpUriRequest request = RequestBuilder.create("DELETE")
+                .setUri("http://localhost:8080/delete/" + BOOK_ID)
+                .build();
+
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+
+        assert bookRepository.findById(BOOK_ID).isEmpty();
+        assert httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK;
     }
 }
